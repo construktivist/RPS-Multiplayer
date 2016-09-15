36002cddbbf565a50da0768a08a4229da7c2b68c
Keegan Kelly <kkelly@ncsoft.corp>
2016-09-15 15:25:03 -0500

    Added inital html code and connected css, javascript, jquery, bootstrap and firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB3xBFPSCmhwHH6DtmoRMKhM25EjYgau1Y",
+    authDomain: "rps-multiplayer-ce727.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-ce727.firebaseio.com",
+    storageBucket: "rps-multiplayer-ce727.appspot.com",
+    messagingSenderId: "127120404327"
+  };
+  firebase.initializeApp(config);
