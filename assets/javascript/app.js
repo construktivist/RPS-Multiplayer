154270e1cf738fabfd18dc23e1d124ce30f4f7a4
Keegan Kelly <kkelly@ncsoft.corp>
2016-09-16 18:23:44 -0500

    Added html elements. Player can enter name and store name, wins and losses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,37 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyB3xBFPSCmhwHH6DtmoRMKhM25EjYgau1Y",
-    authDomain: "rps-multiplayer-ce727.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-ce727.firebaseio.com",
-    storageBucket: "rps-multiplayer-ce727.appspot.com",
-    messagingSenderId: "127120404327"
-  };
-  firebase.initializeApp(config);
+$(document).ready(function(){
+	
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyB3xBFPSCmhwHH6DtmoRMKhM25EjYgau1Y",
+		authDomain: "rps-multiplayer-ce727.firebaseapp.com",
+		databaseURL: "https://rps-multiplayer-ce727.firebaseio.com",
+		storageBucket: "rps-multiplayer-ce727.appspot.com",
+		messagingSenderId: "127120404327"
+	};
+	firebase.initializeApp(config);
+	
+	var database = firebase.database();
+	
+	//Player 1 enters name
+	$("#play-btn").click(function(){
+		var playerName = $("#player-name").val().trim();
+		var playerWins = 0;
+		var playerLosses = 0;
+		
+		console.log(playerName);
+		
+		database.ref().push({
+			name: playerName,
+			wins: playerWins,
+			losses: playerLosses
+		});
+	});
+	
+	
+	
+});
+
+
+
+  
+  
